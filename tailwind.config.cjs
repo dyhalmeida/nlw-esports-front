3331f4f56219df1f4135eb2b557f516f8aeb619a
Diego Almeida <dyhalmeida@gmail.com>
2022-09-13 21:40:51 -0300

    feat: add phosphor-react icon, add Iner font and add layout

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,9 +5,14 @@ module.exports = {
     './index.html'
   ],
   theme: {
+    fontFamily: {
+      sans: ['Inter', 'sans-serif']
+    },
     extend: {
       backgroundImage: {
-        galaxy: "url('/galaxy.png')"
+        galaxy: "url('/galaxy.png')",
+        'nlw-gradient': 'linear-gradient(89.86deg, #9572FC 23.08%, #43E7AD 33.94%, #E1D55D 44.57%)',
+        'game-gradient': 'linear-gradient(180deg, rgba(0, 0, 0, 0), 25%, rgba(0, 0, 0, 0.9) 67.08%)'
       }
     },
   },
