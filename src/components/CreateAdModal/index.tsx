294876889e6aa702ba23b04e1316381c2b46c535
Diego Almeida <dyhalmeida@gmail.com>
2022-09-18 07:53:27 -0300

    feat: add Checkbox

--- src/components/CreateAdModal/index.tsx
+++ src/components/CreateAdModal/index.tsx
@@ -1,5 +1,6 @@
+import * as Checkbox from "@radix-ui/react-checkbox";
 import * as Dialog from "@radix-ui/react-dialog";
-import { GameController } from "phosphor-react";
+import { Check, GameController } from "phosphor-react";
 import { Input } from "../Form/Input";
 
 export function CreateAdModal() {
@@ -74,8 +75,12 @@ export function CreateAdModal() {
             </div>
           </div>
 
-          <div className="mt-2 flex gap-2 text-sm">
-            <Input type="checkbox" id="useVoiceChannel" />
+          <div className="mt-2 flex items-center gap-2 text-sm">
+            <Checkbox.Root className="w-6 h-6 rounded bg-zinc-900 p-1">
+              <Checkbox.Indicator>
+                <Check className="w-4 h-4 text-emerald-400" />
+              </Checkbox.Indicator>
+            </Checkbox.Root>
             <label htmlFor="useVoiceChannel">
               Costumo me conectar ao chat de voz
             </label>
